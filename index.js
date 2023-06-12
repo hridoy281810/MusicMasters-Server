59e68a37f9bdb0174cd9fa4c84ed8dda3a77e942
hridoy281810 <hridoy01724281810@gmail.com>
2023-06-12 13:52:28 +0600

    crate admin feedback classes

--- index.js
+++ index.js
@@ -210,7 +210,7 @@ async function run() {
       const result = await classesCollection.find({status:'approve'}).sort({ number_of_students: -1 }).limit(6).toArray();
       res.send(result)
     })
-    
+
     // home page api 
     app.get('/classes/student', async (req, res) => {
       const result = await classesCollection.aggregate([
@@ -230,7 +230,7 @@ async function run() {
     })
 
 
-    // admin feedback classes 
+    // 
 
     app.post('/feedback', async (req, res) => {
       const feedback = req.body;
